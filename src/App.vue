992b6f1e7f953b7c150cbac2dee231049a760078
xiaoshan <xiaoshan@myai.tech>
2023-03-16 17:12:36 +0800

    文案修改

--- src/App.vue
+++ src/App.vue
@@ -39,7 +39,7 @@
 
         wx.ready(function () {
           var shareData64 = {
-              title: "仟传AI助手-AI助手", //必填,分享标题
+              title: "TargetSocial-AI助手", //必填,分享标题
               desc: "仟传AI助手，启迪灵感，释放效率", //选填,分享描述
               imgUrl: "https://ai-domain-mapping.tarsocial.com/ai-assistant/static/ico/favicon-155ae4ce.ico", //选填,分享图链接
               link: window.location.href, //必填,⽀持直接填写location.href
