32be758b9aa921ba1724165ecb8d780c3581d458
xiaoshan <xiaoshan@myai.tech>
2023-03-21 16:40:49 +0800

    chore(config): cz-customizable配置更改

--- .cz-config.js
+++ .cz-config.js
@@ -53,16 +53,31 @@ module.exports = {
       name: 'merge: 合并分支'
     }
   ],
+  scopes: [
+    ['components', '组件相关'],
+    ['utils', 'utils 相关'],
+    ['ui', '对 ui 的调整'],
+    ['styles', '样式相关'],
+    ['deps', '项目依赖'],
+    ['config', '配置 修改'],
+    ['other', '其他 修改']
+  ].map(([value, description]) => {
+    return {
+      value,
+      name: `${value.padEnd(30)} (${description})`
+    }
+  }),
+  // allowCustomScopes: true,
   messages: {
     type: '请选择提交类型(必填)',
-    customScope: '请输入文件修改范围(可选)',
+    scope: '选择文件修复范围(可选)',
+    customScope: '请输入文件修改范围', // 选择 scope: custom 时会出下面的提示
     subject: '请简要描述提交(必填)',
     body: '请输入详细描述(可选)',
-    breaking: '列出任何BREAKING CHANGES(可选)',
+    breaking: '列举非兼容性重大的变更(可选)',
     footer: '请输入要关闭的issue(可选)',
     confirmCommit: '确定提交此说明吗？'
   },
-  allowCustomScopes: true,
   allowBreakingChanges: ['feat', 'fix'], // 当提交类型为feat、fix时才有破坏性修改选项
   subjectLimit: 72
 }
